0a3475a3b689353f391d4bc95991f434b696eea7
MoriyaSuwako <83826394+MoriyaSuwako2524@users.noreply.github.com>
2023-04-04 21:07:24 +0800

    update generate of output

---
+++ Theoretical Chemistry/Bash for opt/extract.py
@@ -0,0 +1,29 @@
+import sys
+opt_out = open("tem_opt_out.txt","r").read().split("opt.log")
+ene_out = open("tem_ene_out.txt","r").read().split("ene.log")
+output_file = open("summary.csv","w")
+tem_ene_out = []
+tem_opt_out = []
+tem_name_ene = []
+tem_name_opt = []
+for i in range(len(opt_out)):
+    if i == 0 :
+        tem_name_opt.append(opt_out[i].split(" ")[-1])
+        continue
+    tem_float = float(opt_out[i].split(" ")[-2])
+    tem_opt_out.append(tem_float)
+    tem_name_opt.append(opt_out[i].split(" ")[-1])
+
+
+for i in range(len(ene_out)):
+    if i == 0:
+        tem_name_ene.append(ene_out[i].split(" ")[-1])
+        continue
+
+    tem_float = float(ene_out[i].split(" ")[7])
+    tem_ene_out.append(tem_float)
+    tem_name_ene.append(ene_out[i].split(" ")[-1])
+output_file.write("name,correct_to_G,exac_ene,total_G\n")
+for i in range(len(tem_ene_out)):
+    if tem_name_opt[i] == tem_name_ene[i]:
+        output_file.write(str(tem_name_opt[i][0:-1])+","+str(tem_opt_out[i])+","+str(tem_ene_out[i])+","+str(tem_opt_out[i]+tem_ene_out[i])+"\n")
