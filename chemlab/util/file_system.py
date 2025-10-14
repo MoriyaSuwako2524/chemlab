a3f96dfcad928adf95fa7277fd84d6dfab28e2d5
MoriyaSuwako <83826394+MoriyaSuwako2524@users.noreply.github.com>
2025-10-14 17:33:19 -0400

    Update

--- chemlab/util/file_system.py
+++ chemlab/util/file_system.py
@@ -1089,6 +1089,7 @@ class qchem_out_excite(qchem_out):
                 dipole_find = True
             if dipole_find:
                 dipoles = ln.split()
+                print(ln)
                 tx, ty, tz = float(dipoles[0]), float(dipoles[2]), float(dipoles[4])
                 gs.dipole_mom = (tx, ty, tz)
                 dipole_find = False
