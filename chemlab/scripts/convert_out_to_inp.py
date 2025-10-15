e2578999ca790ae8079f35b25619bfec9d916d8c
MoriyaSuwako <83826394+MoriyaSuwako2524@users.noreply.github.com>
2025-10-15 13:01:00 -0400

    Update

--- chemlab/scripts/convert_out_to_inp.py
+++ chemlab/scripts/convert_out_to_inp.py
@@ -2,7 +2,7 @@ import sys
 from chemlab.util.modify_inp import conver_opt_out_to_inp
 def main(filename):
     ref = "ref.in"
-    int = conver_opt_out_to_inp()
-    int.ref_name = ref
-    int.convert(new_out_file_name=filename)
+    inp = conver_opt_out_to_inp()
+    inp.ref_name = ref
+    inp.convert(new_out_file_name=filename)
 main(sys.argv[1])
