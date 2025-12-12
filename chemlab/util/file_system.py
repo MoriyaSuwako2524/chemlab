c405c2e2e6b1daf894bd30e50c744bdfccb0fcbf
MoriyaSuwako2524 <suwako2524@gmail.com>
2025-12-12 13:54:07 -0500

    update

--- chemlab/util/file_system.py
+++ chemlab/util/file_system.py
@@ -374,7 +374,7 @@ class molecule(qchem_inp_block):
         return carti[:, 0]
 
     def replace_new_xyz(self, xyz):
-        atom_type = np.array(self.carti)[:, 0].T
+        atom_type = np.array(self.carti)[:, 0].T.reshape(-1, 1)
         combined = np.hstack([atom_type, xyz])
         self.carti = combined
         return combined
