3ff5f982332997fb1c5113b06206d2ef83c0a723
MoriyaSuwako2524 <suwako2524@gmail.com>
2026-02-03 13:08:39 -0500

    update

--- chemlab/scripts/base.py
+++ chemlab/scripts/base.py
@@ -147,11 +147,12 @@ class QchemBaseScript(Script):
             time.sleep(poll_interval)
 
 def run_qchem_job_async(inp_file, out_file, ncore, env_script, launcher="srun", cache=""):
-
+    workdir = os.path.dirname(inp_file)
     if launcher == "srun":
         cmd = f"""
 {env_script}
 export OMP_NUM_THREADS={ncore}
+cd {workdir}
 srun -n1 -c {ncore} --cpu-bind=cores --hint=nomultithread qchem -nt {ncore} {inp_file} {out_file} {cache}
 """
     else:
